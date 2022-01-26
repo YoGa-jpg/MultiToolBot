66881bf34c5495406c79a915eab7c158dee81995
YoGa-jpg <antondavidovich45@gmail.com>
2022-01-26 16:05:54 +0300

    Config mapper

---
+++ ConfigJson.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace MultiToolBot
+{
+    public class ConfigJson
+    {
+        [JsonProperty("token")]
+        public string Token { get; private set; }
+
+        [JsonProperty("prefix")]
+        public string CommandPrefix { get; private set; }
+
+        [JsonProperty("host")]
+        public string Hostname { get; private set; }
+
+        [JsonProperty("port")]
+        public int Port { get; set; }
+
+        [JsonProperty("pass")]
+        public string Password { get; set; }
+    }
+}
